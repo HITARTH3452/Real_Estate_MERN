d69585a90d19c7dfb1ada9812c13f77521e60955
HITARTH3452 <hitarthsingh2201@gmail.com>
2024-06-18 15:32:54 +0530

    created update user API

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server is up and running on 3000!");
 });
@@ -24,12 +27,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-app.use((err , req , res, next) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
-    success : false,
+    success: false,
     message,
-    statusCode
-  })
-})
+    statusCode,
+  });
+});
